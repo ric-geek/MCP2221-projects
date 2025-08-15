7242c82f1742500eada9ef312b93aea09d33bdbf
Ricky <riccardocasaro@yahoo.it>
2025-08-15 16:52:51 +0200

    - Created "blink" project

---
+++ blink/blink/Program.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Threading;
+using mcp2221_dll_m;
+
+namespace blink
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            uint device = 0;
+            uint pid = 221;
+            uint vid = 1240;
+            int result;
+            byte sram = MCP2221.M_RUNTIME_SETTINGS;
+            byte[] pinFunctions = new byte[4];
+            byte[] pinDirections = new byte[4];
+            byte[] outputValues = new byte[4];
+            byte[] gpioValues = {0, MCP2221.M_NO_CHANGE, MCP2221.M_NO_CHANGE, MCP2221.M_NO_CHANGE };
+
+            result = MCP2221.M_Mcp2221_GetConnectedDevices(vid, pid, ref device);
+            
+            // Check if devices is connected
+            if (result != MCP2221.M_E_NO_ERR)
+            {
+                
+                Console.WriteLine("MCP2221 no connected!");
+
+                return;
+
+            }
+            
+            // Open the communication
+            var handleChip = MCP2221.M_Mcp2221_OpenByIndex(vid, pid, device-1);
+            
+            // Read GPIO configuration
+            result = MCP2221.M_Mcp2221_GetGpioSettings(handleChip, 1, pinFunctions, pinDirections, outputValues);
+
+            pinFunctions[0] = MCP2221.M_MCP2221_GPFUNC_IO; // GP0 set as GPIO
+            pinDirections[0] = MCP2221.M_MCP2221_GPDIR_OUTPUT; // GP0 configured as output
+            outputValues[0] = MCP2221.M_MCP2221_GPVAL_LOW; // GP0 set to low value
+            
+            // Set GPIO configuration
+            result = MCP2221.M_Mcp2221_SetGpioSettings(handleChip, sram, pinFunctions, pinDirections, outputValues);
+
+            if (result != MCP2221.M_E_NO_ERR)
+            {
+                Console.WriteLine("Error {0}", result);
+            }
+            
+            // Set GP0 to high value (LED ON)
+            gpioValues[0] = MCP2221.M_MCP2221_GPVAL_HIGH;
+            
+            result = MCP2221.M_Mcp2221_SetGpioValues(handleChip, gpioValues);
+            
+            if (result != MCP2221.M_E_NO_ERR)
+            {
+                Console.WriteLine("Error {0}", result);
+            }
+            
+            // Wait for 5 seconds
+            Thread.Sleep(5000);
+            
+            // Set GP0 to low value (LED OFF)
+            gpioValues[0] = MCP2221.M_MCP2221_GPVAL_LOW;
+            
+            result = MCP2221.M_Mcp2221_SetGpioValues(handleChip, gpioValues);
+            
+            if (result != MCP2221.M_E_NO_ERR)
+            {
+                Console.WriteLine("Error {0}", result);
+            }
+            
+            // Close communication with the chip
+            result = MCP2221.M_Mcp2221_Close(handleChip);
+            
+            if (result != MCP2221.M_E_NO_ERR)
+            {
+                Console.WriteLine("Error {0}", result);
+            }
+            
+        }
+    }
+}
